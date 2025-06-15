3342b7238c7a9c935cad73ca6645ce9cb6745cc2
venkateswararao-dev <venkateswararaokukkala.y@gmail.com>
2025-06-15 22:57:08 +0530

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "CareerPath Navigator",
+  description: "Empowering Students through Career Exploration",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+      >
+        <Navigation />
+        <main className="flex-grow">
+          {children}
+        </main>
+        <Footer />
+      </body>
+      <Analytics />
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero";
+import FeaturedTools from "./components/FeaturedTools";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <FeaturedTools />
+    </>
+  );
+}
